09948edf93242377ef67f744f39a316da91f7376
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-05-27 10:33:08 +0300

    feat: updated Authorize attribute to check roles and isActive state of account

--- SP.Authorization/AuthorizeAttribute.cs
+++ SP.Authorization/AuthorizeAttribute.cs
@@ -8,15 +8,31 @@ namespace SP.Authorization
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class AuthorizeAttribute : Attribute, IAuthorizationFilter
     {
+        private string[] roles;
+
+        public AuthorizeAttribute()
+        {
+            this.roles = Array.Empty<string>();
+        }
+
+        public virtual string[] Roles
+        {
+            get { return roles; }
+            set { roles = value; }
+        }
+
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             // skip authorization if action is decorated with [AllowAnonymous] attribute
             var isAllowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
             if (isAllowAnonymous) return;
 
-            // check if authorized by JwtMiddleware
-            var isAuthorized = (bool)context.HttpContext.Items["isAuthorized"];
-            if (!isAuthorized) context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            // check if authorized by JwtMiddleware, account is active, and has required roles
+            var accountRoles = (HashSet<string>)context.HttpContext.Items["Roles"];
+            var hasRoles = Roles.All(role => accountRoles.Contains(role));
+            var isActive = (bool)context.HttpContext.Items["IsActive"];
+            var isTokenValid = (bool)context.HttpContext.Items["IsTokenValid"];
+            if (!isTokenValid || !isActive || !hasRoles) context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
         }
     }
 }
