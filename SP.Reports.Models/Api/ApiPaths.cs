f555e4299987d586596cb908ef1c9e9c1d4429ae
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-08 15:05:23 +0300

    chore: add new endpoints

--- SP.Reports.Models/Api/ApiPaths.cs
+++ SP.Reports.Models/Api/ApiPaths.cs
@@ -2,14 +2,16 @@
 {
     public class ApiPaths
     {
-        public string? McpSmp { get; set; }
-        public string? RealTimeGeneration { get; set; }
-        public string? Dpp { get; set; }
-        public string? IntraDayAof { get; set; }
-        public string? IntraDayVolumeSummary { get; set; }
-        public string? Smp { get; set; }
-        public string? DppOrganization { get; set; }
-        public string? DppInjectionUnitName { get; set; }
+        public string McpSmp { get; set; } = String.Empty;
+        public string DayAheadMcp { get; set; } = String.Empty;
+        public string RealTimeGeneration { get; set; } = String.Empty;
+        public string Dpp { get; set; } = String.Empty;
+        public string IntraDayAof { get; set; } = String.Empty;
+        public string IntraDaySummary { get; set; } = String.Empty;
+        public string IntraDayVolumeSummary { get; set; } = String.Empty;
+        public string Smp { get; set; } = String.Empty;
+        public string DppOrganization { get; set; } = String.Empty;
+        public string DppInjectionUnitName { get; set; } = String.Empty;
 
     }
 }
--- SP.Reports.Models/Api/ApiSettings.cs
+++ SP.Reports.Models/Api/ApiSettings.cs
@@ -2,7 +2,7 @@
 {
     public class ApiSettings
     {
-        public string? BaseUrl { get; set; }
+        public string BaseUrl { get; set; } = String.Empty;
         public ApiPaths? Paths { get; set; }
     }
 }
