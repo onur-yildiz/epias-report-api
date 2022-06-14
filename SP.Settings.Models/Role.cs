42bba16113f36dc6a157cdaacd9f8648e202627e
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-14 19:00:07 +0300

    feat: add role model

---
+++ SP.Settings.Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Settings.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Role
+    {
+        [Required]
+        [BsonRequired]
+        [BsonElement("name")]
+        public string Name { get; set; }
+    }
+}
