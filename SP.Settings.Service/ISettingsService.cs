502b5184101397b1b57d04ff591b045fa9cbbfad
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-14 19:01:52 +0300

    chore: remove report methods

--- SP.Settings.Service/ISettingsService.cs
+++ SP.Settings.Service/ISettingsService.cs
@@ -4,7 +4,6 @@ namespace SP.Settings.Service
 {
     public interface ISettingsService
     {
-        IEnumerable<ReportHierarchyItem>? GetReportListingInfo(string? authToken = null);
         Role? GetRole(string role);
         void CreateRole(Role role);
         IEnumerable<Role>? GetRoles();
--- SP.Settings.Service/SettingsService.cs
+++ SP.Settings.Service/SettingsService.cs
@@ -13,48 +13,12 @@ namespace SP.Settings.Service
 {
     public class SettingsService : ISettingsService
     {
-        //readonly IMongoDatabase _db;
         readonly IMongoCollection<BsonDocument> _roles;
-        readonly IMongoCollection<BsonDocument> _reports;
-        readonly IMongoCollection<Account> _users;
-        readonly IJwtUtils _jwtUtils;
 
         public SettingsService(IMongoClient client, IJwtUtils jwtUtils)
         {
             var db = client.GetDatabase("cluster0");
             this._roles = db.GetCollection<BsonDocument>("roles");
-            this._reports = db.GetCollection<BsonDocument>("report-listing-info");
-            this._users = db.GetCollection<Account>("users");
-            this._jwtUtils = jwtUtils;
-        }
-
-        private List<ReportHierarchyItem> FilterRoutes(List<ReportHierarchyItem> routes, string[] userRoles)
-        {
-            routes.RemoveAll(route =>
-            {
-                if (route.Children != null)
-                {
-                    var filteredChildren = FilterRoutes(route.Children.ToList(), userRoles);
-                    if (filteredChildren.Count == 0) return true;
-                    route.Children = filteredChildren;
-                    return false;
-                };
-                if (route.Roles == null) return false;
-                return !route.Roles.All(r => userRoles.Contains(r));
-            });
-            return routes;
-        }
-
-        // TODO DO BETTER
-        public IEnumerable<ReportHierarchyItem>? GetReportListingInfo(string? authToken = null)
-        {
-            var reportsDocs = _reports.Find(_ => true).ToList();
-            var reportHierarchy = reportsDocs.Select(doc => BsonSerializer.Deserialize<ReportHierarchyItem>(doc)).ToList();
-            var userId = authToken != null ? _jwtUtils.ValidateToken(authToken) : null;
-            var userRoles = new List<string>();
-            if (userId != null) userRoles.AddRange(_users.Find(u => u.Id == userId).Project(u => u.Roles).FirstOrDefault());
-            var filteredRoutes = FilterRoutes(reportHierarchy, userRoles.ToArray());
-            return filteredRoutes;
         }
 
         public IEnumerable<Role>? GetRoles()
