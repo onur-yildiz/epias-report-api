02450cce658c5ada1fb1ef35d3fbd48b3a6cac5f
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-14 18:59:36 +0300

    refactor,feat: move report models from Settings.Model to Reports.Models

---
+++ SP.Reports.Models/ReportListing/ReportFolder.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Reports.Models.ReportListing
+{
+    [BsonIgnoreExtraElements]
+    public class ReportFolder
+    {
+        [BsonRequired]
+        [BsonElement("order")]
+        public string Order { get; set; }
+
+        [BsonRequired]
+        [BsonElement("name")]
+        public HashSet<ReportName> Name { get; set; }
+    }
+}
--- SP.Reports.Models/ReportListing/ReportName.cs
+++ SP.Reports.Models/ReportListing/ReportName.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SP.Settings.Models
+namespace SP.Reports.Models.ReportListing
 {
     public class ReportName
     {
