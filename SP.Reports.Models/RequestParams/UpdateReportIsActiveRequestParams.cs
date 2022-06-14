66d78ff0934462bbe7eb263081f0a86dd34e395c
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-14 18:59:59 +0300

    feat: add new models for role and activate requests

---
+++ SP.Reports.Models/RequestParams/UpdateReportIsActiveRequestParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Reports.Models.RequestParams
+{
+    public class UpdateReportIsActiveRequestParams
+    {
+        public string Key { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
