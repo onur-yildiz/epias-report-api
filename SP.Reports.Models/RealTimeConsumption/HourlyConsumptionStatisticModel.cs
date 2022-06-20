27051b226ef9fc958b6402ef9c448b8cd2705b37
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-20 11:33:30 +0300

    feat: add hourly consumption models

---
+++ SP.Reports.Models/RealTimeConsumption/HourlyConsumptionStatisticModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SP.Reports.Models.RealTimeConsumption
+{
+    public class HourlyConsumptionStatisticModel
+    {
+        [JsonPropertyName("consumptionAvg")]
+        public double? ConsumptionAvg { get; set; }
+
+        [JsonPropertyName("consumptionMax")]
+        public double? ConsumptionMax { get; set; }
+
+        [JsonPropertyName("consumptionMin")]
+        public double? ConsumptionMin { get; set; }
+
+        [JsonPropertyName("consumptionSum")]
+        public double? ConsumptionSum { get; set; }
+
+        [JsonPropertyName("date")]
+        public string? Date { get; set; }
+    }
+}
