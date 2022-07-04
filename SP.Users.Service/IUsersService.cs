c1a3d703616b119bb5b2ab1a8e1ffef3fd5318a1
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-07-04 17:49:30 +0300

    feat,refactor: implement DAL for users service, remove unnecessary methods

--- SP.Users.Service/IUsersService.cs
+++ SP.Users.Service/IUsersService.cs
@@ -6,9 +6,7 @@ namespace SP.Users.Service
 {
     public interface IUsersService
     {
-        Account GetAccountById(ObjectId id);
         AuthUser RefreshToken(string token);
-        bool IsAccountExisting(string email);
         AuthUser Login(IUserLoginRequestBody r);
         AuthUser Register(IUserRegisterRequestBody r);
         void UpdateRoles(string userId, IUpdateAccountRolesRequestBody r);
@@ -17,6 +15,5 @@ namespace SP.Users.Service
         string CreateApiKey(string targetUserId);
         void DeleteApiKey(string apiKey, string targetUserId);
         public IEnumerable<User> GetAllUsers();
-        bool CheckIfApiKeyExists(string apiKey);
     }
 }
