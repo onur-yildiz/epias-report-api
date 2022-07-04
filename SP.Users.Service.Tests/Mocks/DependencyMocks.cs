9d615388f42876ac1bc1be483bbfb55745f3945b
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-07-04 18:02:03 +0300

    feat: implement users service tests

---
+++ SP.Users.Service.Tests/Mocks/DependencyMocks.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using SP.DAL.Interfaces;
+using SP.Utils.Cryptography;
+using SP.Utils.Jwt;
+
+namespace SP.Users.Service.Tests.Mocks
+{
+    internal class DependencyMocks
+    {
+        public Mock<IUserRepository> UserRepository = new();
+        public Mock<IApiKeyRepository> ApiKeyRepository = new();
+        public Mock<IJwtUtils> JwtUtils = new();
+        public Mock<ICryptographyUtils> CryptUtils = new();
+    }
+}
